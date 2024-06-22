242d91d127474e0d8ab1016db36f205cb8cb9577
D3VIDE <114795870+D3VIDE@users.noreply.github.com>
2024-06-22 14:52:38 +0700

    success connect db

--- src/main/java/com/example/projectbasisdata/DatabaseConnection.java
+++ src/main/java/com/example/projectbasisdata/DatabaseConnection.java
@@ -2,11 +2,21 @@ package com.example.projectbasisdata;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/projectBD";
     private static final String USER = "postgres";
     private static final String PASSWORD = "P4T5xi";
 
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            System.out.println("Connected to PostgreSQL database!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL,USER,PASSWORD);
     }
