5dc1f8b75d59da18e1c15ed8f371bd3e652d3d7f
Fearkunn <richiedaryl@gmail.com>
2024-06-29 15:11:57 +0700

    Add fx:id for main form and dashboard

---
+++ src/main/java/com/example/projectbasisdata/controller/MainScreenController.java
@@ -0,0 +1,71 @@
+package com.example.projectbasisdata.controller;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class MainScreenController implements Initializable {
+
+    @FXML
+    private AnchorPane main_form;
+    @FXML
+    private Button dashboard_btn;
+    @FXML
+    private Button inventory_btn;
+    @FXML
+    private Button menu_btn;
+    @FXML
+    private Button customer_btn;
+    @FXML
+    private Button promo_btn;
+    @FXML
+    private AnchorPane dashboard_form;
+    @FXML
+    private AreaChart<?, ?> dashboard_incomeChart;
+    @FXML
+    private AreaChart<?, ?> dashboard_customerChart;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
